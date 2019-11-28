7d00052741107496e68516b02a3d127a80b3195e
印聪 <yincong@yincongdeMacBook-Pro.local>
2019-11-28 20:06:01 +0800

    auto commit

---
+++ src/yc/java/Arrays/print1ToMaxOfNDigits_offer17.java
@@ -0,0 +1,16 @@
+package yc.java.Arrays;
+
+/**
+ * @program: Algorithm-Practices
+ * @description: 打印从1到最最大的n位数
+ * @author: yc
+ * @create: 2019-11-28 18:29
+ **/
+
+/**
+ * 由于n可能会非常大，因此不能直接用int表示数字，而是用char数组进行存储
+ * 使用回溯法得到所有的数
+ * */
+
+public class print1ToMaxOfNDigits_offer17 {
+}
---
+++ src/yc/java/Search/restoreIpAddresses_93.java
@@ -0,0 +1,12 @@
+package yc.java.Search;
+
+/**
+ * @program: Algorithm-Practices
+ * @description: ip地址划分
+ * @author: yc
+ * @create: 2019-11-28 19:59
+ **/
+
+
+public class restoreIpAddresses_93 {
+}

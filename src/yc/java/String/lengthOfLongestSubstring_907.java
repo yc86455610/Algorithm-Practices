fe24f3e6db2892d25ae75f128652beb423a04e17
yc86455610 <2698676480@qq.com>
2019-11-29 19:04:47 +0800

    auto commit

---
+++ src/yc/java/String/lengthOfLongestSubstring_907.java
@@ -0,0 +1,56 @@
+package yc.java.String;
+
+import javax.sound.midi.Soundbank;
+import java.sql.SQLOutput;
+import java.util.HashMap;
+
+/**
+ * @program: Algorithm-Practices
+ * @description: 无重复字符的最长子串
+ * @author: yc
+ * @create: 2019-11-29 18:26
+ *
+ * 给定一个字符串，找出不含有重复字符的最长子串的长度。
+ * 示例：
+ * 给定 "abcabcbb" ，没有重复字符的最长子串是 "abc" ，那么长度就是3。
+ * 给定 "bbbbb" ，最长的子串就是 "b" ，长度是1。
+ * 给定 "pwwkew" ，最长子串是 "wke" ，长度是3。请注意答案必须是一个子串，"pwke" 是 子序列 而不是子串
+ *
+ * 思路：使用hashmap记录字符上次出现的位置，用pre记录最近重复字符出现的位置，
+ * 则i(当前位置)-pre就是当前字符最长无重复字符的长度，
+ * 取最大的就是字符串的最长无重复字符的长度
+ **/
+
+
+public class lengthOfLongestSubstring_907 {
+    public static int lengthOfLongestSubstring(String str) {
+        if (str == null || str.length() < 1)
+            return 0;
+
+        //记录字符上次出现的位置
+        HashMap<Character, Integer> map = new HashMap<>();
+        int max = 0;
+
+        //最近一次字符重复出现的位置
+        //i=3  ch='a'  pre=0  表示字符'a'最近一次在0位置出现过
+        int pre = -1;
+
+        for (int i = 0, strLen = str.length(); i < strLen; i++) {
+            Character ch = str.charAt(i);
+            //index是当前字符在hashmap中存在的位置
+            Integer index = map.get(ch);
+            if (index != null)
+                pre = Math.max(pre, index);
+            //i-pre 是当前字符最长无重复字符的长度
+            max = Math.max(max, i - pre);
+            map.put(ch, i);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("abcabcbb"));  //3
+        System.out.println(lengthOfLongestSubstring("pwwkew")); //3
+        System.out.println(lengthOfLongestSubstring("arabcacfr")); //4
+    }
+}
---
+++ src/yc/java/Tree/VerifySquenceOfBST_offer33.java
@@ -0,0 +1,48 @@
+package yc.java.Tree;
+
+/**
+ * @program: Algorithm-Practices
+ * @description: 二叉搜索树的后续遍历序列
+ * @author: yc
+ * @create: 2019-11-29 17:51
+ *
+ * 输入一个整数数组，判断该数组是不是某二叉搜索树的后序遍历的结果。
+ * 假设输入的数组的任意两个数字都互不相同。
+ *
+ * 例如，下图是后序遍历序列 1,3,2 所对应的二叉搜索树。
+ *
+ * 后序遍历(左右根)
+ * 二叉搜索树: 左节点值 < root节点值 < 右节点值
+ **/
+
+
+public class VerifySquenceOfBST_offer33 {
+    public static boolean VerifySquenceOfBST(int[] sequence) {
+        if (sequence == null || sequence.length == 0)
+            return false;
+        return verify(sequence, 0, sequence.length - 1);
+    }
+
+    private static boolean verify(int[] sequence, int first, int last) {
+        //序列只有一个元素
+        if (last - first <= 1)
+            return true;
+        //根节点是序列最后一个元素
+        int rootVal = sequence[last];
+        //cutIndex用来分割左子树和右子树
+        int cutIndex = first;
+        while (cutIndex < last && sequence[cutIndex] <= rootVal)
+            cutIndex++;
+        //验证右子树所有节点是否都大于root节点值
+        for (int i = cutIndex; i < last; i++) {
+            if (sequence[i] < rootVal)
+                return false;
+        }
+        return verify(sequence, first, cutIndex - 1) && verify(sequence, cutIndex, last - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] sequence = {1, 3, 2};
+        System.out.println(VerifySquenceOfBST(sequence));
+    }
+}
